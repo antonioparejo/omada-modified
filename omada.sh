e466e947a74916250fa4532561bf063692e2b298
antonioparejo <88091072+antonioparejo@users.noreply.github.com>
2025-01-02 20:46:02 +0100

    Update omada.sh

--- omada.sh
+++ omada.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/antonioparejo/omada-modified/refs/heads/main/build.func.sh)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
